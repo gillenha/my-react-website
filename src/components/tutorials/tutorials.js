16031d2180dfa5360df3bc28386796d43bb44bc7
gillenha <gillen08@gmail.com>
2018-06-19 18:08:43 -0400

    added youtube tutorials

--- src/components/tutorials/tutorials.js
+++ src/components/tutorials/tutorials.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 import './tutorials.css';
+
 class Tutorials extends Component {
 	render() {
+		const opts = {
+			height: '300',
+			width: '500',
+			playerVars: { 
+				autoplay: 0
+			}
+		}
+
 		return(
 			<div>
 				<p className="section-paragraph">I make Javascript tutorials. Check out how I solve problems!</p>
+				<YouTube 
+					className="w-25 pa3 mr2"
+					videoId="a-IbUyGxewI" 
+					opts={opts} 
+					onReady={this._onReady} 
+				/>
+				<YouTube 
+					className="w-25 pa3 mr2"
+					videoId="PenXAtiFmJc" 
+					opts={opts} 
+					onReady={this._onReady} 
+				/>
+				<YouTube 
+					className="w-25 pa3 mr2"
+					videoId="WvykNpJrmBI" 
+					opts={opts} 
+					onReady={this._onReady} 
+				/>
 			</div>
 		);
+	}
+
+	_onReady(event) {
+		event.target.pauseVideo();
 	}
 }
 
