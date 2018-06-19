60408a6316eb8f528e4e785b8fd0bf0a8519652a
gillenha <gillen08@gmail.com>
2018-06-19 17:48:25 -0400

    touched up fonts

--- src/components/welcome_page/welcome_page.js
+++ src/components/welcome_page/welcome_page.js
@@ -9,9 +9,10 @@ class WelcomePage extends Component {
 				<h1 className="welcome-intro"><strong>Harry Gillen</strong></h1>
 				<p>Web Developer</p>
 				<p>northern Michigan</p>
-				<i className="fab fa-linkedin fa-3x"></i>
-				<i className="fab fa-github fa-3x"></i>
-				<i className="fab fa-twitter fa-3x"></i>
+				<a href="https://www.linkedin.com/in/harrygillen/" target="_blank"><i className="fab fa-linkedin fa-3x"></i></a>
+				<a href="https://github.com/gillenha" target="_blank"><i className="fab fa-github fa-3x"></i></a>
+				<a href="https://twitter.com/HarryHGillen" target="_blank"><i className="fab fa-twitter fa-3x"></i></a>
+				<a href="mailto:gillen08@gmail.com?Subject=Lets%20connect!" target="_top" target="_blank"><i className="fas fa-at fa-3x i-hov"></i></a>
 			</div>
 		)
 	}
