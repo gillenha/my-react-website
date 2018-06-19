edcbd9fdfacf83c4b19fc46b56d77398c51e07a9
gillenha <gillen08@gmail.com>
2018-06-19 14:24:52 -0400

    changed navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,8 +7,8 @@ class Navbar extends Component {
 			<div className="nav-container">
 				<ul className="nav-center">
 					<li><a className="active" href="#home">Home</a></li>
-					<li><a href="#skills">Skills</a></li>
-					<li><a href="#contact">Portfolio</a></li>
+					<li><a href="#skills">Portfolio</a></li>
+					<li><a href="#contact">About</a></li>
 					<li><a href="#about">Contact</a></li>
 				</ul>
 			</div>
